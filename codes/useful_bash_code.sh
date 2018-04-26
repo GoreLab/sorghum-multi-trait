028eb52e20f1031d33b047498164faf398a0a6ad
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-04-26 10:00:55 -0400

    update useful_bash_code

--- codes/useful_bash_code.sh
+++ codes/useful_bash_code.sh
@@ -4,17 +4,13 @@
 ## For firing python scripts into bash code:
 
 # Prefix python:
-PREFIX_python = /workdir/jp2476/software/python/bin
+PREFIX_python=/workdir/jp2476/software/python/bin
 
 # Prefix code:
-PREFIX_code = /workdir/jp2476/repo/sorghum-multi-trait/codes
+PREFIX_code=/workdir/jp2476/repo/sorghum-multi-trait/codes
 
 # Firing the process:
 ${PREFIX_python}/python ${PREFIX_code}/mtrait_data_processing.py & 
-
-
-
-
 
 
 
