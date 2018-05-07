27e26c550578125c71ea3461a1e80ac3b50d541b
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-05-07 09:22:58 -0400

    test on tmp code to add flags to python code

--- codes/tmp.py
+++ codes/tmp.py
@@ -1,8 +1,14 @@
-# Creating folders to store the results:
-bash_line2 = "for i in $(seq 0 " + str(n_alt-1)+ "); do mkdir core"+ str(core) + "_alt${i}_" + structure + "; done;"
 
-# Current core where the analysis is happening:
-core = parser.core
 
-# Number of alternative runs per bin:
-n_alt = parser.alt
+import argparse
+
+parser = argparse.ArgumentParser()
+
+#-db DATABASE -u USERNAME -p PASSWORD -size 20000
+parser.add_argument("-m", "--model", dest = "model", default = "bn", help="Name of the model")
+
+args = parser.parse_args()
+
+print( "Hostname {}".format(
+        args.model,
+        ))
