1c8929e9deb63410a5190ec252e179388cf1270a
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-05-07 12:38:18 -0400

    up

--- codes/tmp.py
+++ codes/tmp.py
@@ -1,15 +1,7 @@
-# Saving height data:
-X['cv1_height_trn'].to_csv('x_cv1_height_trn')
-y['cv1_height_trn'].to_csv('y_cv1_height_trn')
-X['cv1_height_dev'].to_csv('x_cv1_height_dev')
-y['cv1_height_dev'].to_csv('y_cv1_height_dev')
-X['cv1_height_tst'].to_csv('x_cv1_height_tst')
-y['cv1_height_tst'].to_csv('y_cv1_height_tst')
-
 # Saving biomass data:
-X['cv1_biomass_trn'].to_csv()
-y['cv1_biomass_trn'].to_csv()
-X['cv1_biomass_dev'].to_csv()
-y['cv1_biomass_dev'].to_csv()
-X['cv1_biomass_tst'].to_csv()
-y['cv1_biomass_tst'].to_csv()
+X['cv1_biomass_trn'].to_csv('x_cv1_biomass_trn')
+y['cv1_biomass_trn'].to_csv('y_cv1_biomass_trn')
+X['cv1_biomass_dev'].to_csv('x_cv1_biomass_dev')
+y['cv1_biomass_dev'].to_csv('y_cv1_biomass_dev')
+X['cv1_biomass_tst'].to_csv('x_cv1_biomass_tst')
+y['cv1_biomass_tst'].to_csv('y_cv1_biomass_tst')
