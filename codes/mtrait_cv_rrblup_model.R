b040d2f7f99655e44a26bbf8b5488806566037e5
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-05-14 10:03:30 -0400

    up

--- codes/mtrait_cv_rrblup_model.R
+++ codes/mtrait_cv_rrblup_model.R
@@ -413,11 +413,11 @@ for (i in ls(metrics)) {
 save.image("mtrait_cv_rrblup_model.RData")
 
 
-# Setting directory:
-setwd(paste0(prefix_out, "outputs/two_stage_tests_rrblup"))
+# # Setting directory:
+# setwd(paste0(prefix_out, "outputs/two_stage_tests_rrblup"))
 
-# Loading data in future runs:
-load("mtrait_cv_rrblup_model.RData")
+# # Loading data in future runs:
+# load("mtrait_cv_rrblup_model.RData")
 
 #---------------------------------------------------Junk-----------------------------------------------------#
 
