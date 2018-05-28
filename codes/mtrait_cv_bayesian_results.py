efef267de30a96e7d957592c3bbba17d6dcbde32
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-05-28 16:18:32 -0400

    working on process results from dbn

--- codes/mtrait_cv_bayesian_results.py
+++ codes/mtrait_cv_bayesian_results.py
@@ -7,26 +7,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import itertools
 import seaborn as sns
 
 import os
 
-import tensorflow as tf
 import pystan as ps
-import subprocess
-import dill
 import time
-import sys
 import pickle
 import re
 import pprint
+from funcy import project
 
-from scipy.stats import skew
-from scipy.stats import moment
 from scipy.stats.stats import pearsonr
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import normalized_mutual_info_score
 from sklearn.metrics import r2_score
 
@@ -79,14 +71,21 @@ args = parser.parse_args()
 
 ## Temp:
 core = 0
-model = "PBN1"
+model = "DBN"
 cv = "CV1"
-structure = "cv1_biomass_drymass-cv1_height"
+structure = "cv1_height"
+# structure = "cv1_biomass_drymass-cv1_height"
 
 # Seed to recover the analysis:
 seed = core
 
 #--------------------------------------------Reading data----------------------------------------------------#
+
+# Setting the directory:
+os.chdir(prefix_out + "data")
+
+# Loading the data frame with phenotypic data and id's:
+df = pd.read_csv("df.csv", header = 0, index_col=0)
 
 # Setting the directory:
 os.chdir(prefix_out + 'data/cross_validation/' + cv.lower())
@@ -95,74 +94,65 @@ os.chdir(prefix_out + 'data/cross_validation/' + cv.lower())
 y = dict()
 X = dict()
 
-if bool(re.search('-', structure)):
-  # Getting the traits info:
-  struc = pd.Series(structure).str.split("-")[0]
-  for i in range(len(struc)):
-    # Initializing dictionaries:
-    y[struc[i]] = dict()
-    X[struc[i]] = dict()
-    # Loading the data
-    X[struc[i]]['trn'] = pd.read_csv('x_' + struc[i] + '_trn.csv', header = 0, index_col=0)
-    y[struc[i]]['trn'] = pd.read_csv('y_' + struc[i] + '_trn.csv', header = 0, index_col=0)
-    X[struc[i]]['dev'] = pd.read_csv('x_' + struc[i] + '_dev.csv', header = 0, index_col=0)
-    y[struc[i]]['dev'] = pd.read_csv('y_' + struc[i] + '_dev.csv', header = 0, index_col=0)
-    X[struc[i]]['tst'] = pd.read_csv('x_' + struc[i] + '_tst.csv', header = 0, index_col=0)
-    y[struc[i]]['tst'] = pd.read_csv('y_' + struc[i] + '_tst.csv', header = 0, index_col=0)
-    # Biomass:
-    if bool(re.search('-', structure)):
+# Different types of cross-validation sets:
+sets = ['trn', 'dev', 'tst']
+
+for s in sets:
+  if bool(re.search('PBN', model)):
+    # Getting the traits info:
+    struc = pd.Series(structure).str.split("-")[0]
+    for i in range(len(struc)):
+    	if s==sets[0]:
+    		# Initializing dictionaries:
+    		y[struc[i]] = dict()
+    		X[struc[i]] = dict()
+    	# Loading the data
+    	X[struc[i]][s] = pd.read_csv('x_' + struc[i] + '_' + s + '.csv', header = 0, index_col=0)
+    	y[struc[i]][s] = pd.read_csv('y_' + struc[i] + '_' + s + '.csv', header = 0, index_col=0)
+    	# Biomass:
+    	if bool(re.search('biomass', struc[i])):
+    	  # Subsetting just the desired factors:
+    	  index = X[struc[i]][s].columns.str.contains('|'.join(['loc','year']))
+    	  X[struc[i]]['nobin_' + s] = X[struc[i]][s].loc[:,index]
+    	  index = X[struc[i]][s].columns.str.contains('bin')
+    	  X[struc[i]]['bin_' + s] = X[struc[i]][s].loc[:,index]
+    	# Height:
+    	if struc[i]=="cv1_height":
+    	  # Subsetting just the desired factors:
+    	  index = X[struc[i]][s].columns.str.contains('|'.join(['loc', 'year', 'dap']))
+    	  X[struc[i]]['nobin_' + s] = X[struc[i]][s].loc[:,index]
+    	  index = X[struc[i]][s].columns.str.contains('bin')
+    	  X[struc[i]]['bin_' + s] = X[struc[i]][s].loc[:,index]
+  else:
+    # Loading the data:
+    X[s] = pd.read_csv('x_' + structure + '_' + s + '.csv', header = 0, index_col=0)
+    y[s] = pd.read_csv('y_' + structure + '_' + s + '.csv', header = 0, index_col=0)
+    if structure=="cv1_biomass":
       # Subsetting just the desired factors:
-      index = X[struc[i]]['trn'].columns.str.contains('|'.join(['loc','year']))
-      X[struc[i]]['nobin_trn'] = X[struc[i]]['trn'].loc[:,index]
-      index = X[struc[i]]['trn'].columns.str.contains('bin')
-      X[struc[i]]['bin_trn'] = X[struc[i]]['trn'].loc[:,index]
-      index = X[struc[i]]['dev'].columns.str.contains('|'.join(['loc','year']))
-      X[struc[i]]['nobin_dev'] = X[struc[i]]['dev'].loc[:,index]
-      index = X[struc[i]]['dev'].columns.str.contains('bin')
-      X[struc[i]]['bin_dev'] = X[struc[i]]['dev'].loc[:,index]
-      index = X[struc[i]]['tst'].columns.str.contains('|'.join(['loc','year']))
-      X[struc[i]]['nobin_tst'] = X[struc[i]]['tst'].loc[:,index]
-      index = X[struc[i]]['tst'].columns.str.contains('bin')
-      X[struc[i]]['bin_tst'] = X[struc[i]]['tst'].loc[:,index]
-    # Height:
-    if struc[i]=="cv1_height":
+      index = X[s].columns.str.contains('|'.join(['loc','year', 'bin']))
+      X[s] = X[s].loc[:,index]
+    if structure=="cv1_height":
       # Subsetting just the desired factors:
-      index = X[struc[i]]['trn'].columns.str.contains('|'.join(['loc', 'year', 'dap']))
-      X[struc[i]]['nobin_trn'] = X[struc[i]]['trn'].loc[:,index]
-      index = X[struc[i]]['trn'].columns.str.contains('bin')
-      X[struc[i]]['bin_trn'] = X[struc[i]]['trn'].loc[:,index]
-      index = X[struc[i]]['dev'].columns.str.contains('|'.join(['loc', 'year', 'dap']))
-      X[struc[i]]['nobin_dev'] = X[struc[i]]['dev'].loc[:,index]
-      index = X[struc[i]]['dev'].columns.str.contains('bin')
-      X[struc[i]]['bin_dev'] = X[struc[i]]['dev'].loc[:,index]
-      index = X[struc[i]]['tst'].columns.str.contains('|'.join(['loc', 'year', 'dap']))
-      X[struc[i]]['nobin_tst'] = X[struc[i]]['tst'].loc[:,index]
-      index = X[struc[i]]['tst'].columns.str.contains('bin')
-      X[struc[i]]['bin_tst'] = X[struc[i]]['tst'].loc[:,index]
-else:
-  # Loading the data:
-  X['trn'] = pd.read_csv('x_' + structure + '_trn.csv', header = 0, index_col=0)
-  y['trn'] = pd.read_csv('y_' + structure + '_trn.csv', header = 0, index_col=0)
-  X['dev'] = pd.read_csv('x_' + structure + '_dev.csv', header = 0, index_col=0)
-  y['dev'] = pd.read_csv('y_' + structure + '_dev.csv', header = 0, index_col=0)
-  X['tst'] = pd.read_csv('x_' + structure + '_tst.csv', header = 0, index_col=0)
-  y['tst'] = pd.read_csv('y_' + structure + '_tst.csv', header = 0, index_col=0)  
-  if structure=="cv1_biomass":
-    # Subsetting just the desired factors:
-    index = X['trn'].columns.str.contains('|'.join(['loc','year', 'bin']))
-    X['trn'] = X['trn'].loc[:,index]
-    index = X['dev'].columns.str.contains('|'.join(['loc','year', 'bin']))
-    X['dev'] = X['dev'].loc[:,index]
-    index = X['tst'].columns.str.contains('|'.join(['loc','year', 'bin']))
-    X['tst'] = X['tst'].loc[:,index]
-  if structure=="cv1_height":
-    # Subsetting just the desired factors:
-    index = X['trn'].columns.str.contains('|'.join(['loc','year', 'dap', 'bin']))
-    X['trn'] = X['trn'].loc[:,index]
-    index = X['dev'].columns.str.contains('|'.join(['loc','year', 'dap', 'bin']))
-    X['dev'] = X['dev'].loc[:,index]
-    index = X['tst'].columns.str.contains('|'.join(['loc','year', 'dap', 'bin']))
-    X['tst'] = X['tst'].loc[:,index]
+      index = X[s].columns.str.contains('|'.join(['loc','year', 'dap', 'bin']))
+      X[s] = X[s].loc[:,index]
+
+# Preparing training data for the DBN Bayesian Network:
+if model == 'DBN':
+  # DAP indexes:
+  tmp = df.dap.drop_duplicates()[1::] \
+        .astype(int) \
+        .astype(str) \
+        .tolist()
+  # Subsetting just the desired factors:
+  for s in sets:
+  	for t in tmp:
+  	  subset = X[s]['dap'] == float(t)
+  	  index = X[s].columns.str.contains('|'.join(['loc', 'year', 'dap']))
+  	  X[t + '_nobin_' + s] = X[s].loc[subset,index]
+  	  index = X[s].columns.str.contains('bin')
+  	  X[t + '_bin_' + s] = X[s].loc[subset,index]
+  	  y[t + '_' + s] = y[s][subset]
+
 
 #---------------------------------------------Development----------------------------------------------------#
 
@@ -180,7 +170,14 @@ fit = data_dict['fit']
 outs = fit['trn'].extract()
 
 # Getting the predictions:
-if bool(re.search('-', structure)):
+if model=='BN':
+	# Getting the predictions:
+	y_pred = dict()
+	y_pred['trn'] = outs['mu'].mean(axis=0) + X['trn'].dot(outs['beta'].mean(axis=0))
+	y_pred['dev'] = outs['mu'].mean(axis=0) + X['dev'].dot(outs['beta'].mean(axis=0))
+	y_pred['tst'] = outs['mu'].mean(axis=0) + X['tst'].dot(outs['beta'].mean(axis=0))
+
+if bool(re.search('PBN', model)):
 	y_pred = dict()
 	for i in range(len(struc)):
 		# Getting posterior mean of the parameters:
@@ -195,16 +192,42 @@ if bool(re.search('-', structure)):
 		y_pred[struc[i] + '_trn'] = mu_tmp + X[struc[i]]['nobin_trn'].dot(beta_tmp) + X[struc[i]]['bin_trn'].dot(alpha_tmp + eta_tmp)
 		y_pred[struc[i] + '_dev'] = mu_tmp + X[struc[i]]['nobin_dev'].dot(beta_tmp) + X[struc[i]]['bin_dev'].dot(alpha_tmp + eta_tmp)
 		y_pred[struc[i] + '_tst'] = mu_tmp + X[struc[i]]['nobin_tst'].dot(beta_tmp) + X[struc[i]]['bin_tst'].dot(alpha_tmp + eta_tmp)
-else:
+
+if model=='DBN':
 	# Getting the predictions:
 	y_pred = dict()
-	y_pred['trn'] = outs['mu'].mean(axis=0) + X['trn'].dot(outs['beta'].mean(axis=0))
-	y_pred['dev'] = outs['mu'].mean(axis=0) + X['dev'].dot(outs['beta'].mean(axis=0))
-	y_pred['tst'] = outs['mu'].mean(axis=0) + X['tst'].dot(outs['beta'].mean(axis=0))
-
+	# DAP indexes:
+	tmp = df.dap.drop_duplicates()[1::] \
+	      .astype(int) \
+	      .astype(str) \
+	      .tolist()
+	for t in range(len(tmp)):
+		# Getting posterior mean of the parameters:
+		mu_tmp = outs['mu_' + str(t)].mean(axis=0)
+		beta_tmp = outs['beta_' + str(t)].mean(axis=0)
+		alpha_tmp = outs['alpha_' + str(t)].mean(axis=0)
+		d_tmp = outs['d'].mean(axis=0)
+		# Computing predictions for train, dev, and test sets:
+		y_pred[tmp[t] + '_trn'] = mu_tmp + X[tmp[t] + '_nobin_trn']['dap'] * d_tmp + X[tmp[t] + '_nobin_trn'].drop('dap',axis=1).dot(beta_tmp) + X[tmp[t] + '_bin_trn'].dot(alpha_tmp)
+		y_pred[tmp[t] + '_dev'] = mu_tmp + X[tmp[t] + '_nobin_dev']['dap'] * d_tmp + X[tmp[t] + '_nobin_dev'].drop('dap',axis=1).dot(beta_tmp) + X[tmp[t] + '_bin_dev'].dot(alpha_tmp)
+		y_pred[tmp[t] + '_tst'] = mu_tmp + X[tmp[t] + '_nobin_tst']['dap'] * d_tmp + X[tmp[t] + '_nobin_tst'].drop('dap',axis=1).dot(beta_tmp) + X[tmp[t] + '_bin_tst'].dot(alpha_tmp)
 
 # Computing metrics:
-if bool(re.search('-', structure)):
+if model=='BN':
+	# Printing rMSE:
+	round(rmse(y['trn'].values.flatten(), y_pred['trn']), 4)
+	round(rmse(y['dev'].values.flatten(), y_pred['dev']), 4)
+	round(rmse(y['tst'].values.flatten(), y_pred['tst']), 4)
+	# Printing pearsonr:
+	round(pearsonr(y['trn'].values.flatten(), y_pred['trn'])[0], 4)
+	round(pearsonr(y['dev'].values.flatten(), y_pred['dev'])[0], 4)
+	round(pearsonr(y['tst'].values.flatten(), y_pred['tst'])[0], 4)
+	# Printing r2:
+	round(r2_score(y['trn'].values.flatten(), y_pred['trn']), 4)
+	round(r2_score(y['dev'].values.flatten(), y_pred['dev']), 4)
+	round(r2_score(y['tst'].values.flatten(), y_pred['tst']), 4)
+
+if bool(re.search('PBN', model)):
 	rmse_dict = dict()
 	acc_dict = dict()
 	r2_dict = dict()
@@ -228,19 +251,23 @@ if bool(re.search('-', structure)):
 	pprint.pprint(rmse_dict)
 	pprint.pprint(acc_dict)
 	pprint.pprint(r2_dict)
-else:
-	# Printing rMSE:
-	round(rmse(y['trn'].values.flatten(), y_pred['trn']), 4)
-	round(rmse(y['dev'].values.flatten(), y_pred['dev']), 4)
-	round(rmse(y['tst'].values.flatten(), y_pred['tst']), 4)
-	# Printing pearsonr:
-	round(pearsonr(y['trn'].values.flatten(), y_pred['trn'])[0], 4)
-	round(pearsonr(y['dev'].values.flatten(), y_pred['dev'])[0], 4)
-	round(pearsonr(y['tst'].values.flatten(), y_pred['tst'])[0], 4)
-	# Printing r2:
-	round(r2_score(y['trn'].values.flatten(), y_pred['trn']), 4)
-	round(r2_score(y['dev'].values.flatten(), y_pred['dev']), 4)
-	round(r2_score(y['tst'].values.flatten(), y_pred['tst']), 4)
+
+if model=='DBN':
+	for s in ['trn', 'dev', 'tst']:
+		# Index for subsetting the dictionary:
+		index = ['30_' + s, '45_' + s, '60_' + s, '75_' + s, '90_' + s, '105_' + s, '120_' + s]
+		# Subsettting items from the dictionary:
+		tmp = project(y, index)
+		# Stacking prediction and observations observed over time:
+		y_obs_tmp = np.concatenate([tmp[x] for x in tmp], 0)
+		tmp = project(y_pred, index)
+		y_pred_tmp = np.concatenate([tmp[x] for x in tmp], 0)
+		# Printing rMSE:
+		round(rmse(y_obs_tmp, y_pred_tmp), 4)
+		# # Printing pearsonr:
+		# round(pearsonr(y_obs_tmp, y_pred_tmp)[0], 4)
+		# # Printing r2:
+		# round(r2_score(y_obs_tmp, y_pred_tmp), 4)
 
 # Density plots of different data types:
 sns.set_style('whitegrid')
@@ -274,7 +301,6 @@ plt.ylim(0, upper_bound + upper_bound * 0.1)
 plt.show()
 plt.clf()
 
-
 # Bar plot of the biomass bin effects:
 pd.DataFrame(outs['alpha_0'].mean(axis=0), columns=['biomass']) \
     .plot.bar()
@@ -298,7 +324,6 @@ pd.DataFrame(np.abs(outs['z'].mean(axis=0)), columns=['eta']) \
     .plot.bar()
 plt.show()
 plt.clf()
-
 
 # Bar plot of the pleiotropyic bin effects:
 plt.bar(range(outs['z'].mean(axis=0).size), outs['z'].mean(axis=0))
@@ -325,4 +350,29 @@ g1 = (outs['alpha_1'].mean(axis=0) + outs['eta_1'].mean(axis=0))
 pearsonr(W_bin.dot(g0), W_bin.dot(g1))
 plt.scatter(g0,g1)
 plt.show()
+
+
+
+
+
+
+lamb=2.5
+
+indicator = np.empty(outs['z'].shape)
+indicator[:] = np.nan
+
+for s in range(indicator.shape[0]):
+	indicator[s,:] = (outs['z'][s,:] > lamb) | (outs['z'][s,:] < -1 * lamb)
+
+prob = indicator.mean(axis=0)
+
+plt.bar(range(prob.size), prob)
+plt.show()
+plt.clf()
+
+
+
+
+
+
 
