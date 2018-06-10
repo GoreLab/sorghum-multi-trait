d71eb3a409d81ae628b7233db3e23c176065214e
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-06-10 12:41:04 -0400

    working on bn model for run

--- codes/mtrait_bayesian_networks.py
+++ codes/mtrait_bayesian_networks.py
@@ -9,6 +9,8 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import os
+import pickle
+import re
 import pystan as ps
 
 # Prefix of the directory of the project is in:
@@ -26,6 +28,9 @@ from external_functions import *
 
 #---------------------------------------------Loading data---------------------------------------------------#
 
+# Setting the model:
+model = 'BN'
+
 # Setting directory:
 os.chdir(prefix_out + "data/cross_validation")
 
@@ -38,29 +43,57 @@ X = pd.read_csv("x_cv1_height_k0_trn.csv", index_col=0)
 
 #------------------------------------------Data input for stan-----------------------------------------------#
 
-groups = 
-
-dict_stan = dict(n = X.shape[0],
-				 p_x = 1,
-				 p_z = (X.shape[0] - 1),
-
-
-				) 
+if model == 'BN':
+	# Creating an empty list to receive the dictionaries:
+	dict_stan = [] 
+	# Subsetting time indexes and groups:
+	index = X.iloc[:,0].values
+	group = X.iloc[:,0].unique()
+	# Building dictionaries:
+	for t in group:
+		dict_stan.append(dict(n = X[index==t].shape[0],
+						 	  p_z = X[index==t].shape[1],
+						 	  Z = X[index==t],
+						 	  y = y[index==t],
+						 	  phi = np.max(y[index==t]).values*10)) 
 
 # To do list:
-# - Remove heterogeneity of variance into the BN and PBN
-# - Add heterogeneity of variance into the DBN
-# - Change the code to run serial analysis for each DAP measure for BN and PBN
+# - Change the code to run serial analysis for each DAP measure for PBN
 # - Prepare data input for stan for all models
 
-#--------------------------------------Running the Bayesian Network------------------------------------------#
 
+#--------------------------------------Running the Bayesian Network------------------------------------------#
 
 # Setting directory:
 os.chdir(prefix_proj + "codes")
 
-model_stan = ps.StanModel(file='bayesian_network.stan')
+# Compiling the BN model:
+if model == 'BN':
+	model_stan = ps.StanModel(file='bayesian_network.stan')
+	# Creating an empty list:
+	fit = []
+	# Fitting the model:
+	for t in range(len(group)):
+		fit.append(model_stan.sampling(data=dict_stan[t], chains=4, iter=400))
 
-model_stan = ps.StanModel(file='pleiotropic_bayesian_network.stan')
+# Compiling the PBN model:
+if model == 'PBN':
+	model_stan = ps.StanModel(file='pleiotropic_bayesian_network.stan')
 
-model_stan = ps.StanModel(file='dynamic_bayesian_network_0_6.stan')
+# Compiling the DBN model:
+if bool(re.search('DBN', model)):
+	model_stan = ps.StanModel(file='dynamic_bayesian_network_0_6.stan')
+
+
+#---------------------------------Saving outputs from the Bayesian Network-----------------------------------#
+
+# Setting directory:
+os.chdir(prefix_out + 'outputs/cross_validation/' + model.lower())
+
+# Saving stan fit object and model:
+if model == 'BN':
+	for t in range(len(group)):
+		with open('output_' + model.lower() + '_fit_' + str(t) + '.pkl', 'wb') as f:
+		    pickle.dump({'model' : model_stan, 'fit' : fit[t]}, f, protocol=-1)
+
+
