a958ea079c1d94c6758f3c7809afe553a85fd2d0
Jhonathan-Pedroso <jhowpd@gmail.com>
2018-06-28 11:12:09 -0400

    probabilities computed

--- codes/mtrait_results.py
+++ codes/mtrait_results.py
@@ -468,6 +468,65 @@ print(cor_dict['cv2_bn'])
 print(cor_dict['cv2_pbn'])
 print(cor_dict['cv2_dbn'])
 
+# Dictionary to receive the accuracy matrices:
+prob_dict = dict()
+
+# Compute correlation for the Baysian network and Pleiotropic Bayesian Network model under CV2 scheme:
+for k in model_set:
+  for i in range(len(dap_group[:-1])):
+    # Subset predictions and observations for probability computation:
+    y_pred_tmp = y_pred_cv2[k + '_cv2_height_trained!on!dap:' + dap_group[i]]
+    y_obs_tmp = y_obs_cv2['cv2_height_for!trained!on:' + dap_group[i]].y_hat
+    # Computing probability across DAP measures:
+    for j in range(len(dap_group)):
+      # Conditional to compute probability just forward in time:
+      if (j>i):
+        # Subset indexes for subsetting the data:
+        subset = df[df.dap==int(dap_group[j])].index
+        # Get the number of selected individuals for 20% selection intensity:
+        n_selected = int(y_obs_tmp[subset].size * 0.2)
+        # Building the indexes for computing the coeficient of coincidence:
+        rank_obs = np.argsort(y_obs_tmp[subset])[::-1][0:n_selected]
+        # Vector for storing the indicators:
+        ind_vec = pd.DataFrame(index=y_pred_tmp[subset].index, columns=y_pred_tmp[subset].columns)
+        # Build probability matrix for the Bayesian Network model:
+        for sim in range(y_pred_tmp.shape[0]):
+          # Get the indicator of which genotype is in the top 20% or not: 
+          ind_vec.iloc[sim] = np.argsort(y_pred_tmp[subset].iloc[sim])[::-1].isin(rank_obs)
+        # Index to store probabilties into dictionary:
+        index = k + '_' + dap_group[i] + '_' + dap_group[j]
+        # Computing probability:
+        prob_dict[index]=ind_vec.mean(axis=0)
+        print('Model: {}, DAP_i: {}, DAP_j: {}'.format(k, dap_group[i], dap_group[j]))
+
+# Compute correlation for the Baysian network and Pleiotropic Bayesian Network model under CV2 scheme:
+for i in range(len(dap_group1)):
+  # Subsetting predictions for correlation computation:
+  y_pred_tmp = y_pred_cv2['dbn_cv2_height_trained!on!dap:' + dap_group1[i]]
+  y_obs_tmp = y_obs_cv2['cv2_height_for!trained!on:' + dap_group1[i]].y_hat
+  for j in range(len(dap_group2)):    
+    # Getting the upper bound of the interval:
+    upper = int(dap_group1[i].split('~')[1])
+    # Conditional to compute correlation just forward in time:
+    if (int(dap_group2[j])>upper):
+      # Subset indexes for subsetting the data:
+      subset = df[df.dap==int(dap_group2[j])].index
+      # Get the number of selected individuals for 20% selection intensity:
+      n_selected = int(y_obs_tmp[subset].size * 0.2)
+      # Building the indexes for computing the coeficient of coincidence:
+      rank_obs = np.argsort(y_obs_tmp[subset])[::-1][0:n_selected]
+      # Vector for storing the indicators:
+      ind_vec = pd.DataFrame(index=y_pred_tmp[subset].index, columns=y_pred_tmp[subset].columns)
+      # Build probability matrix for the Bayesian Network model:
+      for sim in range(y_pred_tmp.shape[0]):
+        # Get the indicator of which genotype is in the top 20% or not: 
+        ind_vec.iloc[sim] = np.argsort(y_pred_tmp[subset].iloc[sim])[::-1].isin(rank_obs)
+      # Index to store probabilties into dictionary:
+      index = 'dbn_' + dap_group1[i] + '_' + dap_group2[j]
+      # Computing probability:
+      prob_dict[index]=ind_vec.mean(axis=0)
+      print('Model: dbn, DAP_i: {}, DAP_j: {}'.format(dap_group1[i], dap_group2[j]))
+
 # Setting directory:
 os.chdir(prefix_proj + 'plots/cv')
 
@@ -502,6 +561,12 @@ for i in model_set:
   plt.clf()
 
 
+
+
+
+
+
+
 #----------------------------------------------Restore data--------------------------------------------------#
 
 # Load libraries:
@@ -527,7 +592,7 @@ prefix_out = "/workdir/jp2476/repo/resul_mtrait-proj/"
 
 # # Saving data:
 # os.chdir(prefix_out + "outputs/tmp")
-# data = [y, X, y_pred_cv1, y_pred_cv2, y_obs_cv1, y_obs_cv2, df]
+# data = [y, X, y_pred_cv1, y_pred_cv2, y_obs_cv1, y_obs_cv2, prob_dict, df]
 # np.savez('mtrait_results.npz', data)
 
 # Loading data:
--- junk/codes/tmp.py
+++ junk/codes/tmp.py
@@ -1,42 +1,6 @@
 
 
 
-# Store into a list different DAP values:
-dap_group = ['30', '45', '60', '75', '90', '105', '120']
-
-# List of models to use:
-model_set = ['bn', 'pbn']
-
-# Dictionary to receive the accuracy matrices:
-prob_dict = dict()
-
-# Compute correlation for the Baysian network and Pleiotropic Bayesian Network model under CV2 scheme:
-for k in model_set:
-	for i in range(len(dap_group[:-1])):
-		# Subset predictions and observations for probability computation:
-		y_pred_tmp = y_pred_cv2[k + '_cv2_height_trained!on!dap:' + dap_group[i]]
-		y_obs_tmp = y_obs_cv2['cv2_height_for!trained!on:' + dap_group[i]].y_hat
-		# Computing probability across DAP measures:
-		for j in range(len(dap_group)):
-			# Conditional to compute probability just forward in time:
-			if (j>i):
-				# Subset indexes for subsetting the data:
-				subset = df[df.dap==int(dap_group[j])].index
-				# Get the number of selected individuals for 20% selection intensity:
-				n_selected = int(y_obs_tmp[subset].size * 0.2)
-				# Building the indexes for computing the coeficient of coincidence:
-				rank_obs = np.argsort(y_obs_tmp[subset])[::-1][0:n_selected]
-				# Vector for storing the indicators:
-				ind_vec = pd.DataFrame(index=y_pred_tmp[subset].index, columns=y_pred_tmp[subset].columns)
-				# Build probability matrix for the Bayesian Network model:
-				for sim in range(y_pred_tmp.shape[0]):
-					# Get the indicator of which genotype is in the top 20% or not: 
-					ind_vec.iloc[sim] = np.argsort(y_pred_tmp[subset].iloc[sim])[::-1].isin(rank_obs)
-				# Index to store probabilties into dictionary:
-				index = k + '_' + dap_group[i] + '_' + dap_group[j]
-				# Computing probability:
-				prob_dict[index]=ind_vec.mean(axis=0)
-				print('Model: {}, DAP_i: {}, DAP_j: {}'.format(k, dap_group[i], dap_group[j]))
 
 
 
@@ -49,4 +13,57 @@ for k in model_set:
 
 
 
-																																																																																																																																					
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
