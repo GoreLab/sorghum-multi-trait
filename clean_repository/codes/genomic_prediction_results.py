6cc0cfded0f5a05307e8df869255d5d55f8777a3
Jhonathan Pedroso Rigal dos Santos <jhonathan@pop-os.localdomain>
2019-05-12 11:42:26 -0300

    up

--- clean_repository/codes/genomic_prediction_results.py
+++ clean_repository/codes/genomic_prediction_results.py
@@ -794,44 +794,6 @@ for i in range(len(model_set)):
   plt.savefig("heatplot_fcv_" + model_set[i] + "_accuracy.png", dpi=350)
   plt.clf()
 
-
-
-# Generate accuracy heatmaps:
-for i in model_set:
-  # Labels for plotting the heatmap for the Pleiotropic Bayesian Network or Bayesian Network:
-  if (i=='bn') | (i=='pbn'):
-    labels_axis0 = ['DAP 45*', 'DAP 60*', 'DAP 75*', 'DAP 90*', 'DAP 105*']
-  # Labels for plotting the heatmap for the Dynamic Bayesian model:
-  if i=='dbn':
-    labels_axis0 = ['DAP 30:45*', 'DAP 30:60*', 'DAP 30:75*', 'DAP 30:90*', 'DAP 30:105*']
-  # Labels for plotting the heatmap:
-  labels_axis1 = ['DAP 60', 'DAP 75', 'DAP 90', 'DAP 105', 'DAP 120']
-  # Heat map of the adjusted means across traits:
-  heat = sns.heatmap(np.flip(np.flip(cor_dict['fcv_' + i],axis=1), axis=0),
-             linewidths=0.25,
-             cmap='YlOrBr',
-             vmin=0.3,
-             vmax=1,
-             annot=True,
-             annot_kws={"size": 18},
-             xticklabels=labels_axis0 ,
-             yticklabels=labels_axis1)
-  heat.set_ylabel('')    
-  heat.set_xlabel('')
-  plt.xticks(rotation=25)
-  plt.yticks(rotation=45)
-  plt.savefig("heatplot_fcv_" + i + "_accuracy.pdf", dpi=350)
-  plt.savefig("heatplot_fcv_" + i + "_accuracy.png", dpi=350)
-  plt.clf()
-
-
-
-
-
-
-
-
-
 # Set directory:
 os.chdir(REPO_PATH + "/clean_repository/tables")
 
